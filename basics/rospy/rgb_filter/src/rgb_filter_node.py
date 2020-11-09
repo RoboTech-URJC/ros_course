5604d5965282a960e45c70b19defe034eef620d1
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-11-10 00:26:01 +0100

    [rgb_filter] rgb_filter_node.py finished

--- basics/rospy/rgb_filter/src/rgb_filter_node.py
+++ basics/rospy/rgb_filter/src/rgb_filter_node.py
@@ -1,15 +1,49 @@
 #! /usr/bin/env python3
 
 import rospy
+from sensor_msgs.msg import Image
 
+from cv_bridge import CvBridge
+import cv2
 import sys
 
 class RGBFilter():
     def __init__(self):
-        pass
+        self.initParams_()
+        self.rgb_img_ = None
+        self.cv_bridge = CvBridge()
+
+        self.image_sub_ = rospy.Subscriber(self.image_topic_, Image, self.imageCb_)
+
+    def __del__(self):
+        cv2.destroyAllWindows()
 
     def step(self):
-        rospy.loginfo("Step!")
+        if (self.rgb_img_ == None):
+            return
+
+        # sensor_msgs/Image to OpenCV image:
+
+        try:
+            cv_img = self.cv_bridge.imgmsg_to_cv2(self.rgb_img_, self.rgb_img_.encoding)
+        except CvBridgeError as e:
+            rospy.logerr(e)
+            return
+
+        # Resize the image:
+
+        img_resized = cv2.resize(cv_img, (640, 480), interpolation = cv2.INTER_AREA)
+
+        # Show the image:
+
+        cv2.imshow("RGB Image", img_resized)
+        cv2.waitKey(1)
+
+    def imageCb_(self, msg):
+        self.rgb_img_ = msg
+
+    def initParams_(self):
+        self.image_topic_ = rospy.get_param("image_sub", "camera/rgb/image_raw")
 
 def main(args=None):
     rospy.init_node("rgb_filter_node")
